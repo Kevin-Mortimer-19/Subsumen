d33f06616e1e0d340d0213906531ae3f1d4ff805
donaldhurld <donaldhurld@gmail.com>
2021-06-13 00:22:37 -0400

    Stages made
    Stage 2 has issue where 2nd set of doors starts open, too tired to fix

--- Bunji/scripts/Goal.gd
+++ Bunji/scripts/Goal.gd
@@ -4,4 +4,4 @@ extends Area2D
 
 func _on_Goal_body_entered(body):
 	if body.is_in_group("Player"):
-		get_tree().change_scene("res://scenes/End Screen.tscn")
+		get_tree().change_scene("res://scenes/Stage 2.tscn")
