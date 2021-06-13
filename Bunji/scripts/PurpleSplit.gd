19515361df89df1d88f7475c2e9ae758c4f64fcb
Kevin-Mortimer-19 <70980411+Kevin-Mortimer-19@users.noreply.github.com>
2021-06-13 02:37:47 -0400

    purple split works
    it works

--- Bunji/scripts/PurpleSplit.gd
+++ Bunji/scripts/PurpleSplit.gd
@@ -8,12 +8,14 @@ var BLUE = load("res://sprites/C2.png")
 func _on_PurpleSplit_body_entered(body):
 	if body.is_in_group("Player"):
 		var c1 = MAN.instance()
+		body.get_parent().add_child(c1)
 		c1.get_node("Sprite").texture = RED
-		c1.global_position = global_position - Vector2(64,0)
+		c1.global_position = global_position + Vector2(-96,-32)
 		c1.character_number = 2
 		
 		var c2 = MAN.instance()
+		body.get_parent().add_child(c2)
 		c2.get_node("Sprite").texture = BLUE
-		c2.global_position = global_position + Vector2(64,0)
+		c2.global_position = global_position + Vector2(32,-32)
 		c2.character_number = 1
 		body.queue_free()
